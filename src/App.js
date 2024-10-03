c3504fed0f4bd08ab3b2a40f43b76d090735b497
DanCoDesign <dancodesign@proton.me>
2024-10-03 12:54:18 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { BrowserProvider } from './contexts/Web3Context'
 
 function App() {
   return (
-    <div className="flex h-screen justify-center items-center bg-gray-900">
-      <div className="max-w-lg w-full p-6 bg-gray-800 rounded-lg shadow-md text-white">
-        <h1 className="text-2xl font-semibold text-center mb-4">Web3 Wallet Connector</h1>
+    <div className="flex flex-col relative min-h-screen justify-center items-center overflow-hidden bg-gray-50">
+      <h1 className="text-2xl font-semibold text-center mb-4">Simple Metamask Wallet Connector</h1>
+      <div class="absolute -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur opacity-25"></div>
+      
+
         <BrowserProvider>
           <WalletConnector />
         </BrowserProvider>
 
-      </div>
+      
     </div>
   );
 }
